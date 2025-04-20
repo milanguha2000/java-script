0afd79f589ff89e3d4f5daf01f9909f2a45409a1
MILAN GUHA <milanguha2000@gmail.com>
2025-04-20 06:10:34 +0000

    basics variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 12345
+let accountEmail = "milan@gmail.com"
+var accountPassword = "0123456"
+
+accountCity = "Burdwan"
+let accountState ;
+
+// accountId = 0123
+console.log(accountId)
+accountEmail = "mg@gmail.com"
+accountPassword = "123456789"
+accountCity = "bardhaman"
+
+console.table([accountEmail , accountId , accountPassword , accountCity , accountState])
+
