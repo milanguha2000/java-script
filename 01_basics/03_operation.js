4eeef747d3cd6f1c0552d54f058bac64430ccd77
MILAN GUHA <milanguha2000@gmail.com>
2025-04-21 06:09:49 +0000

    operation

---
+++ 01_basics/03_operation.js
@@ -0,0 +1,38 @@
+let value = 3
+let negValue = -value
+console.log(negValue);
+/*
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);
+*/
+let str1 = "hello"
+let str2 = " Milan"
+let str3 = str1 + str2
+console.log(str3);
+
+/*
+console.log("1" + 2);   //12
+console.log(1 + "2");   //12
+console.log("1" + 2 + 2);   //122
+console.log(1 + 2 + "2");   //32
+*/
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+
+let gameCounter1 = 100
+++gameCounter1
+console.log(gameCounter1);
+
+let a = 3
+const b = a++
+console.log(`a:${a} , b:${b}`);
+
+let p = 3
+const q = ++p
+console.log(`p:${p} , q:${q}`);
+
