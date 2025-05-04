ea2a935d743530cca1a42d7e592ee1274e649ed9
MILAN GUHA <milanguha2000@gmail.com>
2025-05-04 05:43:02 +0000

    this and arrow

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -17,3 +17,60 @@ username value is : "sam" . so now current content is "sam". so line no 11 print
 
 
 //console.log(this);     //in this line output will be : {} , because there is no current content globally.
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// "this" keyword only work on object . "this" keyword does not work on function
+
+
+// function chai(){
+//     username : "milan"
+//     console.log(this.username);
+// }
+// chai()       //output : undefined
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// if we store the function in a variable , in same case "this" keyword is not workable
+
+// const chai = function(){
+//     username : "milan"
+//     console.log(this.username);
+// }
+// chai()       //output : undefined
+
+
+// ++++++++++++++++++++++++++++ Arrow Function +++++++++++++++++++++++++++++++++++++
+// arrow function synta. :   () => {}
+const chai = () => {
+    username : "milan"
+    console.log(this.username);
+}
+//chai()      //output : undefined
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// const addTwo = (num1 , num2) => {
+//     return num1 + num2
+// }
+// console.log(addTwo(3 , 4));
+
+// const addTwo = (num1 , num2) => num1 + num2
+// console.log(addTwo(3 , 4));
+
+const addTwo = (num1 , num2) => (num1 + num2)
+console.log(addTwo(3 , 4));
+
+/*line no (52-55) & (57-58) & (60-61) all are same . we can use the arrow function same way.
+   in line no (52-55) , if we use arrow function in curly bracket then you have to "return" value ,
+   otherwise we don't have to use "return"
+*/
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// in arrow function if we use the object
+
+const add = (num1 , num2) => ({user : "milan"})
+console.log(add());        //output : {user : 'milan'}
+// if we use object then you have to rap up curly bracket in a bracket like line no 73 .
+
