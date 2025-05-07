06628cca7aae526b710e20420b7ea3a245d7292f
MILAN GUHA <milanguha2000@gmail.com>
2025-05-07 04:07:21 +0000

    while and do-while loop

---
+++ 05_loops/02_whileAndDo-While.js
@@ -0,0 +1,26 @@
+// while loop
+
+let i = 0
+while (i <= 10) {
+    console.log(`value of i is ${i}`);
+    i = i + 2
+}
+
+
+let myArray = ["batman" , "ironman" , "superman" , "spiderman"]
+
+let arr = 0
+while (arr < myArray.length) {
+    console.log(`value is ${myArray[arr]}`);
+    arr = arr + 1
+}
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//do-while loop
+
+let score = 1
+do {
+    console.log(`score is ${score}`);
+    score ++
+} while (score <= 10);
