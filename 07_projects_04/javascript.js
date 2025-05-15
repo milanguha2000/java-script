2bc4b330b1a2efd40de91d775413a4b6b6d9aedb
MILAN GUHA <milanguha2000@gmail.com>
2025-05-15 05:51:13 +0000

    Guess Number Game

--- 07_projects_04/javascript.js
+++ 07_projects_04/javascript.js
@@ -1,4 +1,4 @@
-const randomNumber = parseInt((Math.random()*100)+1)
+let randomNumber = parseInt((Math.random()*100)+1)
 // console.log(randomNumber);
 
 const submit = document.querySelector('#submit')
@@ -18,7 +18,8 @@ let playGame = true
 if(playGame){
     submit.addEventListener('click' , function(e){
         e.preventDefault()
-        const guess = parseInt(userInput.value)
+        const guess = Math.floor(userInput.value)
+        console.log(guess);
         validateGuess(guess)
     })
 
@@ -31,7 +32,7 @@ function validateGuess(guess){
         alert('Please enter a number less than 100')
     }else{
         prevGuess.push(guess)
-        if(numOfGuess === 11){
+        if(numOfGuess === 10){
             displayGuess(guess)
             displayMessage(`Game Over . Random Number was ${randomNumber}`)
             endGame()
@@ -53,16 +54,33 @@ function checkGuess(guess){
 }
 function displayGuess(guess){
     userInput.value = ''
-    guessSlot.innerHTML += `${guess}`
+    guessSlot.innerHTML += `${guess} , `
     numOfGuess++
-    remaining.innerHTML = `10 - ${numOfGuess}`
+    remaining.innerHTML = `${11 - numOfGuess}`
 }
 function displayMessage(message){
     lowOrHigh.innerHTML = `<h2>${message}</h2>`
 }
 function endGame(){
-
+    userInput.value = ''
+    userInput.setAttribute('disabled' , '')
+    p.classList.add('button')
+    p.innerHTML = '<h2 id = "newGame">Start new Game</h2>'
+    startOver.appendChild(p)
+    playGame = false
+    newGame()
 }
 function newGame(){
+    const newGameButton = document.querySelector('#newGame')
+    newGameButton.addEventListener('click' , function(e){
+        randomNumber = parseInt((Math.random()*100)+1)
+        prevGuess = []
+        numOfGuess = 1
+        guessSlot.innerHTML = ''
+        remaining.innerHTML = `${11 - numOfGuess}`
+        userInput.removeAttribute('disabled')
+        startOver.removeChild(p)
 
+        playGame = true
+    })
 }}
